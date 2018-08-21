211c4892be007df2c0541fa0ddfa9e70375b7474
Thibault Maekelbergh <thibault@macbook046.local>
2018-08-21 16:15:49 +0200

    Fix a bash param bug

--- sketchtool.sh
+++ sketchtool.sh
@@ -5,21 +5,23 @@ set -e
 function check_file_input() {
   if [ $# -eq 0 ]; then
     echo "No arguments provided"
-    echo "Please provide a sketchfile to export!"
-    echo "e.g: npm run export MyFile.sketch"
+    echo "Please provide a sketchfile to export and an output dir!"
+    echo "e.g: bash ./sketchtool.sh MyFile.sketch /var/sketchxport/assets/images"
 
     exit 1
   fi
+
+  if [ -z "$2" ]; then OUTPUT_DIR="$PWD/assets/images"; else OUTPUT_DIR=$2; fi
 }
 
 function export_assets() {
   if [ -f /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool ]; then
     echo "Exporting slices as svg & png(1x,2x,3x) to directory $2"
 
-    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$2" \
+    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$OUTPUT_DIR" \
       --format="png" --scales="1, 2, 3"
 
-    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$2" \
+    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$OUTPUT_DIR" \
       --format="svg"
   else
     echo "💩  sketchtool was not found"
