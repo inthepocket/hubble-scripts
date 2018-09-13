107deb9c726c81e05eefaadd830324654d85e129
thibmaek <thibault.maekelbergh@icloud.com>
2018-09-13 13:32:44 +0200

    Provide new flag useColorArtboards, provide defaults, better help message

--- cli.js
+++ cli.js
@@ -4,20 +4,27 @@ const app = require('./index');
 
 const cli = meow(`
   Usage
-  $ npm start <input> --outputDir="/home/usr/downloads"
+    $ npm start <input> --outputDir="/home/usr/downloads"
 
   Options
-  --outputDir, The directory where parsed files will be placed after a run
-  --dump, Dump all Sketch JSON files into 1 logdump.json
+    --outputDir=<dir>, -o     The directory where parsed files will be placed after a run
+    --dump, -d                Dump all Sketch JSON files into 1 logdump.json
+    --useColorArtboards       Use artboards named "primitives/color/<name>" to export colors instead of using the document colors
 `, {
   flags: {
     outputDir: {
       default: __dirname,
       type: 'string',
+      alias: 'o'
     },
     dump: {
       type: 'boolean',
+      alias: 'd'
     },
+    useColorArtboards: {
+      type: 'boolean',
+      default: false,
+    }
   },
 });
 
