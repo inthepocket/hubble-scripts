d7906857fb125b4704ddf81354a0f50877d98f6a
thibmaek <thibault.maekelbergh@icloud.com>
2018-09-13 18:12:05 +0200

    Make help a bit friendlier

--- cli.js
+++ cli.js
@@ -4,12 +4,16 @@ const app = require('./index');
 
 const cli = meow(`
   Usage
-    $ npm start <input> --outputDir="/home/usr/downloads"
+    $ sketchxport-cli <input> --outputDir="/home/usr/downloads"
 
   Options
-    --outputDir=<dir>, -o     The directory where parsed files will be placed after a run
+    --outputDir=<dir>, -o     The directory where parsed files will be placed after a run. Defaults to current working directory
     --dump, -d                Dump all Sketch JSON files into 1 logdump.json
     --useColorArtboards       Use artboards named "primitives/color/<name>" to export colors instead of using the document colors
+
+  Examples
+    $ sketchxport-cli "__mocks/sample_sketchfile.sketch"
+    $ sketchxport-cli "__mocks__/sample_sketchfile.sketch" -d --useColorArtboards --outputDir="config/"
 `, {
   flags: {
     outputDir: {
