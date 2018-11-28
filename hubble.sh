aae845dbdeae298cd51a48ec8802f30a25359d5c
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-11-28 16:42:11 +0100

    Correctly name shell scrips this time

--- hubble.sh
+++ hubble.sh
@@ -6,36 +6,56 @@ function log() {
   printf "\\n\\033[1m\\033[34m%s\\033[0m\\n\\n" "[hubble-scripts] ${1}"
 }
 
-function check_file_input() {
-  if [ $# -eq 0 ]; then
-    echo "No arguments provided"
-    echo "Please provide a sketchfile to export and an output dir!"
-    echo "e.g: bash ./sketchtool.sh MyFile.sketch /var/hubble/assets/images"
-
-    exit 1
-  fi
-
-  if [ -z "$2" ]; then OUTPUT_DIR="$PWD/assets/images"; else OUTPUT_DIR=$2; fi
+# Generate config file from hubble
+function generate_config() {
+  log "💎  Generating config from sketchfile..."
+  node cli.js "$SKETCHFILE" --outputDir="$CONFIG_OUTPUT_DIR" "$@"
 }
 
-function export_assets() {
-  if [ -f /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool ]; then
-    log "Exporting slices as svg & png(1x,2x,3x) to directory $2"
+# Generate assets from hubble
+function generate_assets() {
+  log "💎  Exporting & moving assets from sketchfile..."
 
-    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$OUTPUT_DIR" \
-      --format="png" --scales="1, 2, 3"
-
-    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$OUTPUT_DIR" \
-      --format="svg"
-  else
-    log "💩  sketchtool was not found"
-    log "Please install Sketch.app and follow https://developer.sketchapp.com/guides/sketchtool/"
-    exit 1
+  if [ ! -d "$ASSETS_OUTPUT_DIR" ]; then
+    log "Asset output dir does not exist yet. Creating it"
+    mkdir -p "$ASSETS_OUTPUT_DIR"
   fi
+
+  bash ./sketchtool.sh "$SKETCHFILE" "$ASSETS_OUTPUT_DIR"
+}
+
+function upload_to_cloud() {
+  # You can enable custom cloud providers here to upload generated output to that provider
+  # upload_to_gcloud "$ASSETS_OUTPUT_DIR" "$CONFIG_OUTPUT_DIR" "$1"
+  exit 0
 }
 
 function main() {
-  check_file_input "$@"
-  export_assets "$@"
+  # Check if we got a sketchfile, otherwise exit 1
+  if [ -z "$1" ]; then
+    echo "No sketchfile provided"
+    echo "Please provide a sketchfile to export!"
+    echo "e.g: ./hubble.sh \"/home/usr/file.sketch\""
+    exit 1
+  fi
+
+  SKETCHFILE=$1
+  if [ -z "$2" ]; then CONFIG_OUTPUT_DIR="$PWD"; else CONFIG_OUTPUT_DIR=$2; fi
+  if [ -z "$3" ]; then ASSETS_OUTPUT_DIR="$PWD/assets/images"; else ASSETS_OUTPUT_DIR=$3; fi
+
+  # Source cloud providers
+  for file in ./services/upload/*; do
+    # shellcheck disable=SC1090,SC1091
+    [ -r "$file" ] && [ -f "$file" ] && source "$file"
+  done
+
+  log "Sketchfile is $SKETCHFILE"
+  log "Config output dir is $CONFIG_OUTPUT_DIR"
+  log "Asset output dir is $ASSETS_OUTPUT_DIR"
+
+  generate_config "$@"
+  generate_assets "$@"
+
+  upload_to_cloud "hubble-output-dir"
 }
 main "$@"
---
+++ sketchtool.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -e
+
+# Pretty log output
+function log() {
+  printf "\\n\\033[1m\\033[34m%s\\033[0m\\n\\n" "[hubble-scripts] ${1}"
+}
+
+function check_file_input() {
+  if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    echo "Please provide a sketchfile to export and an output dir!"
+    echo "e.g: bash ./sketchtool.sh MyFile.sketch /var/hubble/assets/images"
+
+    exit 1
+  fi
+
+  if [ -z "$2" ]; then OUTPUT_DIR="$PWD/assets/images"; else OUTPUT_DIR=$2; fi
+}
+
+function export_assets() {
+  if [ -f /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool ]; then
+    log "Exporting slices as svg & png(1x,2x,3x) to directory $2"
+
+    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$OUTPUT_DIR" \
+      --format="png" --scales="1, 2, 3"
+
+    /Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool export slices "$1" --output="$OUTPUT_DIR" \
+      --format="svg"
+  else
+    log "💩  sketchtool was not found"
+    log "Please install Sketch.app and follow https://developer.sketchapp.com/guides/sketchtool/"
+    exit 1
+  fi
+}
+
+function main() {
+  check_file_input "$@"
+  export_assets "$@"
+}
+main "$@"
