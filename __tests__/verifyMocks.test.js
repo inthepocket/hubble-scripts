b2d33b6b54f29f38140400fc8bc27d462ac4ee21
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-02-15 15:08:45 +0100

    Harden tests for API changes and generate mocks on the CI server

---
+++ __tests__/verifyMocks.test.js
@@ -0,0 +1,13 @@
+const sketchDump = require('../__mocks__/sample_dump.json');
+const hubbleOutput = require('../__mocks__/sample_output.json');
+
+/**
+ * This is a very basic snapshot-diffing test to harness
+ * ourselves against external dependency or Sketch API changes.
+ */
+describe('Verify if the mocks are unchanged', () => {
+  it('should match previous output + dump after generating mocks', () => {
+    expect(sketchDump).toMatchSnapshot();
+    expect(hubbleOutput).toMatchSnapshot();
+  });
+});
