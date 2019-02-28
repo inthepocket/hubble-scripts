fa6c20fe4ddb250675014d3957e8d74c00d3db4a
Sven Hoskens <sven.hoskens@inthepocket.mobi>
2019-02-28 17:34:29 +0100

    Added style dictionary flag to the cli

--- cli.js
+++ cli.js
@@ -7,10 +7,11 @@ const cli = meow(`
     $ hubble-cli <input> --outputDir="/home/usr/downloads"
 
   Options
-    --outputDir=<dir>, -o     The directory where parsed files will be placed after a run. Defaults to current working directory
-    --dump, -d                Dump all Sketch JSON files into 1 logdump.json
-    --useColorArtboards       Use artboard formatting to export colors instead of using the document colors
-    --useGradientArtboards    Use artboard formatting to export gradients instead of using the document gradients
+    --outputDir=<dir>, -o           The directory where parsed files will be placed after a run. Defaults to current working directory
+    --dump, -d                      Dump all Sketch JSON files into 1 logdump.json
+    --useColorArtboards             Use artboard formatting to export colors instead of using the document colors
+    --useGradientArtboards          Use artboard formatting to export gradients instead of using the document gradients
+    --useStyleDictionaryOutput, -s  Generate Style Dictionary compatible output in addition to the regular itp tokens.
 
   Examples
     $ hubble-cli "__mocks/sample_sketchfile.sketch"
@@ -33,6 +34,11 @@ const cli = meow(`
     useGradientArtboards: {
       type: 'boolean',
       default: false,
+    },
+    useStyleDictionaryOutput: {
+      type: 'boolean',
+      default: false,
+      alias: 's'
     },
     version: {
       type: "boolean",
