d66db228441abea84542cb00a5034a527af41c22
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-04-02 11:28:55 +0200

    Split up tests and fix correct paths

--- __tests__/verifyMocks.test.js
+++ __tests__/verifyMocks.test.js
@@ -1,17 +1,23 @@
 const sketchDump = require('../__mocks__/sample_dump.json');
 const hubbleOutput = require('../__mocks__/sample_output.json');
-const hubbleOutputAsDocument = require('../__mocks__/sample_output.json');
-const hubbleOutputAsStyleDictionary = require('../__mocks__/sample_output.json');
+const hubbleOutputAsDocument = require('../__mocks__/sample_output.document.json');
+const hubbleOutputAsStyleDictionary = require('../__mocks__/sample_output.styledictionary.json');
 
 /**
  * This is a very basic snapshot-diffing test to harness
  * ourselves against external dependency or Sketch API changes.
  */
-describe('Verify if the mocks are unchanged', () => {
-  it('should match previous output + dump after generating mocks', () => {
+describe('Verify that the mocks are unchanged', () => {
+  it('for the Sketch dump', () => {
     expect(sketchDump).toMatchSnapshot();
+  });
+  it('for the hubble output (using artboards)', () => {
     expect(hubbleOutput).toMatchSnapshot();
+  });
+  it('for the hubble output (using document styles)', () => {
     expect(hubbleOutputAsDocument).toMatchSnapshot();
+  });
+  it('for the hubble output (using Style Dictionary)', () => {
     expect(hubbleOutputAsStyleDictionary).toMatchSnapshot();
   });
 });
