68d4216370d6aa7e9ef8c2f8a917398ba5d8897f
Cedric Vanhaverbeke <cedric.vanhaverbeke@inthepocket.mobi>
2019-07-09 11:20:50 +0200

    Fix review issues

--- cli.js
+++ cli.js
@@ -2,7 +2,8 @@ const meow = require('meow');
 
 const app = require('./index');
 
-const cli = meow(`
+const cli = meow(
+  `
   Usage
     $ hubble-cli <input> --outputDir="/home/usr/downloads"
 
@@ -12,40 +13,46 @@ const cli = meow(`
     --useColorArtboards             Use artboard formatting to export colors instead of using the document colors
     --useGradientArtboards          Use artboard formatting to export gradients instead of using the document gradients
     --useStyleDictionaryOutput, -s  Generate Style Dictionary compatible output instead of the generic design token format.
+    --token, -t                     Authorization token when accessing the Figma API
 
   Examples
     $ hubble-cli "My Library.sketch"
     $ hubble-cli "My Library.sketch" -d --useColorArtboards --outputDir="config/"
-`, {
-
-  flags: {
-    outputDir: {
-      default: __dirname,
-      type: 'string',
-      alias: 'o'
+`,
+  {
+    flags: {
+      token: {
+        type: 'string',
+        alias: 't',
+      },
+      outputDir: {
+        default: __dirname,
+        type: 'string',
+        alias: 'o',
+      },
+      dump: {
+        type: 'boolean',
+        alias: 'd',
+      },
+      useColorArtboards: {
+        type: 'boolean',
+        default: false,
+      },
+      useGradientArtboards: {
+        type: 'boolean',
+        default: false,
+      },
+      useStyleDictionaryOutput: {
+        type: 'boolean',
+        default: false,
+        alias: 's',
+      },
+      version: {
+        type: 'boolean',
+        alias: 'v',
+      },
     },
-    dump: {
-      type: 'boolean',
-      alias: 'd'
-    },
-    useColorArtboards: {
-      type: 'boolean',
-      default: false,
-    },
-    useGradientArtboards: {
-      type: 'boolean',
-      default: false,
-    },
-    useStyleDictionaryOutput: {
-      type: 'boolean',
-      default: false,
-      alias: 's'
-    },
-    version: {
-      type: "boolean",
-      alias: 'v',
-    }
   },
-});
+);
 
 app(cli.input, cli.flags);
--- index.js
+++ index.js
@@ -2,35 +2,43 @@ const fs = require('fs');
 const pkg = require('./package.json');
 const getParser = require('./lib/parser');
 const getMappers = require('./lib/mappers');
-const { prettyJSON} = require('./lib/utils');
+const { prettyJSON, getExtension } = require('./lib/utils');
 const mapToStyleDictionaryTokens = require('./lib/styleDictionary');
 
-const getExtension = (file) => file.split('.').pop();
-exports.getExtension = getExtension;
-
-module.exports = async(args, flags) => {
+module.exports = async (args, flags) => {
   if (flags.version) return pkg.version;
 
   if (args.length <= 0) throw new Error('No file input passed after npm start');
-  const [file] = args
-  const {parser, response} = await getParser(args, flags);
+  const [file] = args;
+  const { parser } = await getParser(args, flags);
+  const {
+    textStyles,
+    colors,
+    gradients,
+    shadows,
+    borders,
+    fonts,
+    version,
+    response,
+  } = await parser.getTokens();
+
   const mappers = getMappers(getExtension(file));
-  
+
   const mapping = {
-    textStyles: mappers.mapTextStyles(parser.getTextStyles()),
-    colors: mappers.mapColors(parser.getColors()),
-    gradients: mappers.mapGradients(parser.getGradients()),
-    shadows: mappers.mapShadows(parser.getShadows()),
-    borders: mappers.mapBorders(parser.getBorders()),
-    fonts: parser.getFonts(),
-    ...parser.getVersion()
-  }
+    textStyles: mappers.mapTextStyles(textStyles),
+    colors: mappers.mapColors(colors),
+    gradients: mappers.mapGradients(gradients),
+    shadows: mappers.mapShadows(shadows),
+    borders: mappers.mapBorders(borders),
+    fonts,
+    version,
+  };
 
   if (!fs.existsSync(flags.outputDir)) {
     fs.mkdirSync(flags.outputDir);
   }
 
-  const fsErrorHandler = (err) => {
+  const fsErrorHandler = err => {
     if (err) {
       console.error('Error trying to write to file:', err); // eslint-disable-line no-console
       throw new Error(err);
@@ -51,4 +59,4 @@ module.exports = async(args, flags) => {
     await fs.writeFile(`${flags.outputDir}/logdump.json`, prettyJSON(response), fsErrorHandler);
   }
   return response;
-}
+};
