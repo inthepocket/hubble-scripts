bd18f69f174bc1f3377e0f1a438c230bb56d90ed
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-08-01 16:58:15 +0200

    Update README + --help

--- cli.js
+++ cli.js
@@ -5,19 +5,23 @@ const app = require('./index');
 const cli = meow(
   `
   Usage
-    $ hubble-cli <input> --outputDir="/home/usr/downloads"
+    $ hubble-cli <input> [options]
 
   Options
     --outputDir=<dir>, -o           The directory where parsed files will be placed after a run. Defaults to current working directory.
-    --dump, -d                      Dump all Sketch JSON files into 1 logdump.json.
+    --dump, -d                      Dump raw file output into logdump.json.
+                                      For Sketch this is the JSON inside the .sketch file bundle,
+                                      for Figma this is the received REST API response.
     --useColorArtboards             Use artboard formatting to export colors instead of using the document colors.
     --useGradientArtboards          Use artboard formatting to export gradients instead of using the document gradients.
     --useStyleDictionaryOutput, -s  Generate Style Dictionary compatible output instead of the generic design token format.
     --token, -t                     Authorization token when accessing the Figma API.
 
   Examples
-    $ hubble-cli "My Library.sketch"
-    $ hubble-cli "My Library.sketch" -d --useColorArtboards --outputDir="config/"
+    $ hubble-cli "__mocks/sample_sketchfile.sketch"
+    $ hubble-cli "__mocks/sample_sketchfile.sketch" -d --useColorArtboards --outputDir="config/"
+    $ hubble-cli --token "29-206..." HbgJuBVOwIOcoZMVnpG01LqA
+    $ hubble-cli --token "29-206..." HbgJuBVOwIOcoZMVnpG01LqA --useStyleDictionary
 `,
   {
     flags: {
